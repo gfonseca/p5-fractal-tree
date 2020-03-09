4543edc9f1eb95f3bd84a71c792648b3437be7c1
Georgio Barbosa da Fonseca <georgio.fonseca@dft-sp-wkn1137.dafiti.local>
2020-03-09 15:29:11 -0300

    feat(main.js) add smooth effect in tree swing

--- main.js
+++ main.js
@@ -1,45 +1,63 @@
 import * as p5 from 'p5'
 
-let s = (sk) => {
-	sk.swing = 0
-	sk.swingDir = 1
-	sk.setup = () => {
-		sk.createCanvas(sk.windowWidth, sk.windowHeight);
-		sk.stroke("#FFF")
-		sk.slider = sk.createSlider(0, sk.TWO_PI, sk.PI/4, 0.01)
-		console.log(sk.slider)
-	}
-	
-	sk.draw = () => {
-		sk.background(30, 20 ,30);	
-		const len = sk.windowHeight / 3
-		sk.translate(sk.width / 2, sk.height +40)
-		sk.branch(len)
-		
-		sk.swing += 0.001 * sk.swingDir
-		if (sk.swing > 0.02 || sk.swing < 0 ) {
-			sk.swingDir = sk.swingDir * -1
-		}
-		 
-	}
+const s = (sk) => {
+  sk.swing = 0
+  sk.swingDir = 1
+  sk.swingSpeed = 0.002
+  sk.recursionDepth = 8
 
-	sk.branch = len => {
-			sk.strokeWeight(10 * (len / 100))
-			sk.stroke(255, 50, 255 * (len / 100))
-			sk.line(0, 0, 0, -len)
-			sk.translate(0, -len)
-			if(len > 4) {
-				sk.push()
-				sk.rotate(sk.slider.value()+sk.swing)
-				sk.branch(len*0.70)
-				sk.pop()
-				sk.push()
-				sk.rotate(-sk.slider.value()+sk.swing)
-				sk.branch(len*0.70)
-				sk.pop()				
-			}
-	}
+  sk.swingSpeedSlider = null
+
+  sk.drawRect = () => {
+    sk.fill(sk.rectColor)
+    sk.noStroke()
+    sk.rect(0, 0, sk.width, sk.height)
+  }
+
+  sk.sliderValue = () => {
+    sk.swingSpeed = sk.swingSpeedSlider.value() / 100000
+    sk.recursionDepth = sk.depthSlider.value()
+  }
+
+  sk.setup = () => {
+    sk.createCanvas(sk.windowWidth, sk.windowHeight)
+    sk.stroke('#FFF')
+    sk.angleSlider = sk.createSlider(0, sk.TWO_PI, sk.PI / 4, 0.01)
+    sk.swingSpeedSlider = sk.createSlider(1, 500)
+    sk.depthSlider = sk.createSlider(4, 16)
+    sk.rectColor = sk.color(30, 20, 30)
+    sk.rectColor.setAlpha(40)
+  }
+
+  sk.draw = () => {
+    sk.sliderValue()
+    sk.drawRect()
+    const len = sk.windowHeight / 3
+    sk.translate(sk.width / 2, sk.height + 40)
+    sk.branch(len)
+
+    sk.swing += sk.swingSpeed * sk.swingDir
+    if (sk.swing > 0.02 || sk.swing < -0.02) {
+      sk.swingDir = sk.swingDir * -1
+    }
+  }
+
+  sk.branch = len => {
+    sk.strokeWeight(10 * (len / 100))
+    sk.stroke(255, 50, 255 * (len / 100))
+    sk.line(0, 0, 0, -len)
+    sk.translate(0, -len)
+    if (len > sk.recursionDepth) {
+      sk.push()
+      sk.rotate(sk.angleSlider.value() + sk.swing)
+      sk.branch(len * 0.70)
+      sk.pop()
+      sk.push()
+      sk.rotate(-sk.angleSlider.value() + sk.swing)
+      sk.branch(len * 0.70)
+      sk.pop()
+    }
+  }
 }
-
 
 const P5 = new p5(s)
